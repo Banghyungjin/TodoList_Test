701359b8cdd269a473f9395c2af33b6b40ae16cc
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-08-16 16:13:19 +0900

    2021-08-16_01

--- app/src/main/java/com/example/todolist_test/MainActivity.kt
+++ app/src/main/java/com/example/todolist_test/MainActivity.kt
@@ -14,20 +14,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        todoAdapter = TodoAdapter(mutableListOf())
         val recyclerView = findViewById<RecyclerView>(R.id.rView_todo_items)
         val addBtn = findViewById<Button>(R.id.btn_add_todo)
         val delBtn = findViewById<Button>(R.id.btn_del_done)
         val todoTitle = findViewById<EditText>(R.id.todo_title)
 
+        todoAdapter = TodoAdapter(mutableListOf())
         recyclerView.adapter = todoAdapter
         recyclerView.layoutManager = LinearLayoutManager(this)
-
         addBtn.setOnClickListener {
-            val todoTitle_str = todoTitle.text.toString()
-            if(todoTitle_str.isNotEmpty()) {
-                val todo = Todo(todoTitle_str)
+            val todoTitleStr = todoTitle.text.toString()
+            if(todoTitleStr.isNotEmpty()) {
+                val todo = Todo(todoTitleStr)
                 todoAdapter.addTodo(todo)
                 todoTitle.text.clear()
             }
